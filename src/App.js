71f82d10f3e0f8386dd2d0bcc96a9da045ea58ef
Ralph <ralphlaurencevalle28@gmail.com>
2026-04-18 11:18:55 +0800

    TEST

--- src/App.js
+++ src/App.js
@@ -8,56 +8,45 @@ import Project from './components/Projects/projects';
 import Skill from './components/Skills/Skills';
 import CF from './components/CForm/CForm';
 import Footer from './components/Footer/Footer';
-import { useState, useRef } from 'react';
 import Swal from 'sweetalert2';
 import ScrollToTop from './components/ScrollToTop/scrolltotop';
 
-
 function App() {
-
-  // useState
-  // const [count, counter] = useState(0);
-
-  //   const add = () => {
-  //     counter(count + 1);      
-  // }
-  // const minus = () => {
-  //   counter(count - 1);
-  // }
-  // const resets = () => {
-  //   counter(0);
-  // }
-
 
   useEffect(() => {
     Swal.fire({
       title: "Hi!",
       text: "Welcome to my Portfolio",
-      imageUrl: "../logo.png",
+      imageUrl: "/logo.png",
       imageWidth: 200,
       imageHeight: 200,
-      imageAlt: "Custom image"
+      imageAlt: "Logo"
     });
   }, []);
 
-
   return (
     <>
-    <StickyHeader />
-    <Hero />
-    <div className='' style={{display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', gap: '10px'}}>
-      <input type='hidden' autoFocus />
-      
-    </div>
-    <ScrollToTop />
-    <About />
-    <Project />
-    <Skill />
-    <CF />
-    <Footer />
-  </>
+      <StickyHeader />
+      <Hero />
+
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        gap: '10px'
+      }}>
+        <input type='hidden' autoFocus />
+      </div>
+
+      <ScrollToTop />
+      <About />
+      <Project />
+      <Skill />
+      <CF />
+      <Footer />
+    </>
   );
 }
 
 export default App;
-
