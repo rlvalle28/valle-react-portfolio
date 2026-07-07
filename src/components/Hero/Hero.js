1f2c73db4e32faa34f791a3b640d6e194b7b2b0b
Ralph <ralphlaurencevalle28@gmail.com>
2026-07-07 13:00:10 +0800

    TEST

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,7 +6,7 @@ import hero_img from '../../images/ralph-rbg.png';
 import TypingText from '../TypingText/TypingText';
 import { FaFacebook, FaGithub, FaSkype, FaDiscord } from "react-icons/fa";
 import { IoLogoWhatsapp } from "react-icons/io";
-import { FaArrowRight } from "react-icons/fa6";
+// import { FaArrowRight } from "react-icons/fa6";
 
 const handleViewResume = () => {
     const filePath = '/ralphlaurencevalle.pdf';
